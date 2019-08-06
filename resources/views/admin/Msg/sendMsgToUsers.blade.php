5196698e0d145b2806b2aa1d3e4f2fb2b6f2c2f6
laoWang <909535659@qq.com>
2019-08-06 13:36:53 +0800

    修改部分内容

--- resources/views/admin/Msg/sendMsgToUsers.blade.php
+++ resources/views/admin/Msg/sendMsgToUsers.blade.php
@@ -2,53 +2,53 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-<meta http-equiv="X-UA-Compatible" content="IE=emulateIE7" />
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name ="csrf-token" content="{{ csrf_token() }}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/WdatePicker.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/skin_/table.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/skin_/index.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/jquery.grid.css')}}" />
-<link rel="stylesheet" type="text/css" href="{{asset('css/mystyle.css')}}" />
+    <meta http-equiv="X-UA-Compatible" content="IE=emulateIE7" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/WdatePicker.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/skin_/table.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/skin_/index.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/jquery.grid.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/mystyle.css')}}" />
 
-<title>添加产品</title>
+    <title>添加产品</title>
 </head>
 
 <body>
-< div id="container" class="position">
-< div id="hd"></ div >
-< div id="bd">
-< div id="main">
-<form action ="{{url('sendMsgToUser')}}" method="post" style="width: 100%; float: left" enctype="multipart/form-data">
-<input style="display: none" name ="id" value ="{{$info -> id}}" />
-< div class="search-area">
-< div class="kv-item ue-clear">
-<label>反馈内容:</label>
-< div class="kv-item-content">
-{{--<input type="text" style="width: 100px;height: 28px;" name="username" value="{{$info -> phone}}"/>--}}
-{{$info -> remark}}
-</ div >
-</ div >
-</ div >
+    < div id="container" class="position">
+        < div id="hd"></ div>
+        < div id="bd">
+            < div id="main">
+                <form action="{{url('sendMsgToUser')}}" method="post" style="width: 100%; float: left" enctype="multipart/form-data">
+                    <input style="display: none" name="id" value="{{$info -> id}}" />
+                    < div class="search-area">
+                        < div class="kv-item ue-clear">
+                            <label>反馈内容:</label>
+                            < div class="kv-item-content">
+                                {{--<input type="text" style="width: 100px;height: 28px;" name="username" value="{{$info -> phone}}"/>--}}
+                                {{$info -> remark}}
+                            </ div>
+                        </ div>
+                    </ div>
 
 
-< div class="search-area">
-< div class="kv-item ue-clear">
-<label>回复</label>
-< div class="kv-item-content">
-<textarea style="width: 100px;height: 28px;" rows ="3" cols="20" name ="answer"></textarea>
-</ div >
-</ div >
-</ div >
-< div class="search-button" style="margin-left: 20px; margin-top: 30px">
-<input class="button" type="button" onclick="add()" value ="通过" />
-</ div >
-{{--</div>--}}
-</form>
-</ div >
-</ div >
-</ div >
+                    < div class="search-area">
+                        < div class="kv-item ue-clear">
+                            <label>回复</label>
+                            < div class="kv-item-content">
+                                <textarea style="width: 100px;height: 28px;" rows="3" cols="20" name="answer"></textarea>
+                            </ div>
+                        </ div>
+                    </ div>
+                    < div class="search-button" style="margin-left: 20px; margin-top: 30px">
+                        <input class="button" type="button" onclick="add()" value="通过" />
+                    </ div>
+                    {{--</div>--}}
+                </form>
+            </ div>
+        </ div>
+    </ div>
 </body>
 <script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/global.js')}}"></script>
@@ -62,56 +62,56 @@
 <script type="text/javascript" src="{{asset('js/layui/layui.all.js')}}"></script>
 <script type="text/javascript" src="{{asset('js/jquery.form.js')}}"></script>
 <script type="text/javascript">
-// 选择图片显示
-function imgChange(obj) {
-//获取点击的文本框
-var file = document.getElementById("file");
-var imgUrl = window.URL.createObjectURL( file.files[0]);
-var img = document.getElementById('imghead');
-img.setAttribute('src', imgUrl);
-// 修改img标签src属性值
-}
+    // 选择图片显示
+    function imgChange(obj) {
+        //获取点击的文本框
+        var file = document.getElementById("file");
+        var imgUrl = window.URL.createObjectURL(file.files[0]);
+        var img = document.getElementById('imghead');
+        img.setAttribute('src', imgUrl);
+        // 修改img标签src属性值
+    }
 
-function updateStatus( status, id) {
-$.get('updateUserStatus', {
-'status':status,
-'id':id
-}, function ( data ) {
-if ( data.code == 0) {
-layer.msg( data.msg, {
-icon:1
-}, function () {
-var index = parent.layer.getFrameIndex( window.name);
-parent.layer.close( index );
-window.parent.location.reload();
-//刷新父页面
-});
-} else {
-layer.msg( data.msg, {
-icon:2
-});
-}
-})
-}
+    function updateStatus(status, id) {
+        $.get('updateUserStatus', {
+            'status': status,
+            'id': id
+        }, function(data) {
+            if (data.code == 0) {
+                layer.msg(data.msg, {
+                    icon: 1
+                }, function() {
+                    var index = parent.layer.getFrameIndex(window.name);
+                    parent.layer.close(index);
+                    window.parent.location.reload();
+                    //刷新父页面
+                });
+            } else {
+                layer.msg(data.msg, {
+                    icon: 2
+                });
+            }
+        })
+    }
 
-function add () {
-$('form').ajaxSubmit( function ( data ) {
-if ( data.code == 0) {
-layer.msg( data.msg, {
-icon:1
-}, function () {
-var index = parent.layer.getFrameIndex( window.name);
-parent.layer.close( index );
-window.parent.location.reload();
-//刷新父页面
-});
-} else {
-layer.msg( data.msg, {
-icon:2
-});
-}
-});
-}
+    function add() {
+        $('form').ajaxSubmit(function(data) {
+            if (data.code == 0) {
+                layer.msg(data.msg, {
+                    icon: 1
+                }, function() {
+                    var index = parent.layer.getFrameIndex(window.name);
+                    parent.layer.close(index);
+                    window.parent.location.reload();
+                    //刷新父页面
+                });
+            } else {
+                layer.msg(data.msg, {
+                    icon: 2
+                });
+            }
+        });
+    }
 </script>
 
 </html>
